02ae057a6115764360a1b064c3994da7b0acf027
dqmdz <daniel.quinterospinto@gmail.com>
2024-09-29 18:36:43 -0300

    Factura Improved

--- src/main/java/eterea/report/api/rest/service/facade/MakeFacturaService.java
+++ src/main/java/eterea/report/api/rest/service/facade/MakeFacturaService.java
@@ -5,6 +5,7 @@ import eterea.report.api.rest.kotlin.model.dto.ClienteMovimientoDto;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Service
 @Slf4j
@@ -33,26 +35,14 @@ public class MakeFacturaService {
     public String send(Long clienteMovimientoId, String email) throws MessagingException {
         // Genera PDF
         String filenameFactura = facturaPdfService.generatePdf(clienteMovimientoId);
-        log.info("filenameFactura -> " + filenameFactura);
+        log.info("filenameFactura -> {}", filenameFactura);
         if (filenameFactura.isEmpty()) {
             return "ERROR: Sin Factura para ENVIAR";
         }
 
-        String data = "";
-
         ClienteMovimientoDto clienteMovimiento = clienteMovimientoClient.findByClienteMovimientoId(clienteMovimientoId);
 
-        data = "Estimad@ cliente:" + (char) 10;
-        data = data + (char) 10;
-        data = data + "Le enviamos como archivo adjunto su factura." + (char) 10;
-        data = data + (char) 10;
-        data = data + "Le agradecemos habernos elegido." + (char) 10;
-        data = data + (char) 10;
-        data = data + "Atentamente." + (char) 10;
-        data = data + (char) 10;
-        data = data + (char) 10
-                + "Por favor no responda este mail, fue generado automáticamente. Su respuesta no será leída."
-                + (char) 10;
+        String data = getTextForEmail();
 
         // Envia correo
         MimeMessage message = javaMailSender.createMimeMessage();
@@ -60,7 +50,7 @@ public class MakeFacturaService {
         var addresses = new ArrayList<String>();
         var addresses_bcc = new ArrayList<String>();
 
-        if (!clienteMovimiento.getCliente().getEmail().isEmpty()) {
+        if (!Objects.requireNonNull(clienteMovimiento.getCliente()).getEmail().isEmpty()) {
             addresses.add(clienteMovimiento.getCliente().getEmail());
         }
         if (!email.isEmpty()) {
@@ -77,7 +67,7 @@ public class MakeFacturaService {
             helper.setBcc(addresses_bcc.toArray(new String[0]));
             helper.setText(data);
             helper.setReplyTo("no-reply@gmail.com");
-            helper.setSubject("Envío Automático de Factura -> " + filenameFactura);
+            helper.setSubject("Envío Automático de Comprobante -> " + filenameFactura);
 
             FileSystemResource fileRecibo = new FileSystemResource(filenameFactura);
             helper.addAttachment(filenameFactura, fileRecibo);
@@ -87,6 +77,22 @@ public class MakeFacturaService {
         }
         javaMailSender.send(message);
         return "Envío de correo Ok";
+    }
+
+    @NotNull
+    private static String getTextForEmail() {
+        String data = "Estimad@ cliente:" + (char) 10;
+        data = data + (char) 10;
+        data = data + "Le enviamos como archivo adjunto su comprobante." + (char) 10;
+        data = data + (char) 10;
+        data = data + "Le agradecemos habernos elegido." + (char) 10;
+        data = data + (char) 10;
+        data = data + "Atentamente." + (char) 10;
+        data = data + (char) 10;
+        data = data + (char) 10
+                + "Por favor no responda este mail, fue generado automáticamente. Su respuesta no será leída."
+                + (char) 10;
+        return data;
     }
 
 }
